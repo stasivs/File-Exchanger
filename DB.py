d958b4fb5d3c404c46c22e3f81553f346aee0b75
Stasivs <stasivs@bk.ru>
2019-03-09 22:38:36 +0300

    Add DB and some template

---
+++ DB.py
@@ -0,0 +1,109 @@
+import sqlite3
+
+
+class DB:
+    def __init__(self):
+        conn = sqlite3.connect('news.db', check_same_thread=False)
+        self.conn = conn
+
+    def get_connection(self):
+        return self.conn
+
+    def __del__(self):
+        self.conn.close()
+
+
+class UserModel:
+    def __init__(self, connection):
+        self.connection = connection
+        self.init_table()
+
+    def init_table(self):
+        cursor = self.connection.cursor()
+        cursor.execute('''CREATE TABLE IF NOT EXISTS users 
+                            (id INTEGER PRIMARY KEY AUTOINCREMENT, 
+                             user_name VARCHAR(50),
+                             password_hash VARCHAR(128)
+                             )''')
+        cursor.close()
+        self.connection.commit()
+
+    def insert(self, user_name, password_hash):
+        cursor = self.connection.cursor()
+        cursor.execute('''INSERT INTO users 
+                          (user_name, password_hash) 
+                          VALUES (?,?)''', (user_name, password_hash))
+        cursor.close()
+        self.connection.commit()
+
+    def get(self, user_id):
+        cursor = self.connection.cursor()
+        cursor.execute("SELECT * FROM users WHERE id = ?", (str(user_id),))
+        row = cursor.fetchone()
+        return row
+
+    def get_all(self):
+        cursor = self.connection.cursor()
+        cursor.execute("SELECT * FROM users")
+        rows = cursor.fetchall()
+        return rows
+
+    def delete(self, news_id):
+        cursor = self.connection.cursor()
+        cursor.execute('''DELETE FROM users WHERE id = ?''', (str(news_id),))
+        cursor.close()
+        self.connection.commit()
+
+    def exists(self, user_name, password_hash):
+        cursor = self.connection.cursor()
+        cursor.execute("SELECT * FROM users WHERE user_name = ? AND password_hash = ?",
+                       (user_name, password_hash))
+        row = cursor.fetchone()
+        return (True, row[0]) if row else (False,)
+
+
+class Archives:
+    def __init__(self, connection):
+        self.connection = connection
+        self.init_table()
+
+    def init_table(self):
+        cursor = self.connection.cursor()
+        cursor.execute('''CREATE TABLE IF NOT EXISTS archives 
+                            (id INTEGER PRIMARY KEY AUTOINCREMENT, 
+                             title VARCHAR(100),
+                             url VARCHAR(100),
+                             user_id INTEGER
+                             )''')
+        cursor.close()
+        self.connection.commit()
+
+    def insert(self, user_id, title, url):
+        cursor = self.connection.cursor()
+        cursor.execute('''INSERT INTO news 
+                          (user_id, title, url) 
+                          VALUES (?,?,?)''', (str(user_id), title, url))
+        cursor.close()
+        self.connection.commit()
+
+    def get(self, news_id):
+        cursor = self.connection.cursor()
+        cursor.execute("SELECT * FROM news WHERE id = ?", (str(news_id),))
+        row = cursor.fetchone()
+        return row
+
+    def get_all(self, user_id=None):
+        cursor = self.connection.cursor()
+        if user_id:
+            cursor.execute("SELECT * FROM news WHERE user_id = ?",
+                           (str(user_id),))
+        else:
+            cursor.execute("SELECT * FROM news")
+        rows = cursor.fetchall()
+        return rows
+
+    def delete(self, news_id):
+        cursor = self.connection.cursor()
+        cursor.execute('''DELETE FROM news WHERE id = ?''', (str(news_id),))
+        cursor.close()
+        self.connection.commit()
